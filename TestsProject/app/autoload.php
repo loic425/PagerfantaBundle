fb4a76ce8cc43750c9f0a62392350d6ad7c966a9
Michael Babker <michael.babker@gmail.com>
2019-12-18 23:21:36 -0600

    Pull out the test application used to validate rendered views, the translated views don't create any new templates so there isn't a need to validate the rendered HTML (unit tests on the view classes should assert translations are used)

--- TestsProject/app/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$loader = require(__DIR__.'/../../vendor/autoload.php');
-$loader->add('', __DIR__.'/../src');
-
-require_once __DIR__.'/AppKernel.php';
--- TestsProject/src/BabDev/PagerfantaTestBundle/BabDevPagerfantaTestBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BabDev\PagerfantaTestBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BabDevPagerfantaTestBundle extends Bundle
-{
-}
