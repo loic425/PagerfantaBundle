e4c75963f6de76db3dc1c1b3ff9ecd1c4f20ca67
Michael Babker <michael.babker@gmail.com>
2020-04-11 00:02:26 -0500

    Add a compiler pass that removes the Twig view if Twig is not available in the application

--- BabDevPagerfantaBundle.php
+++ BabDevPagerfantaBundle.php
@@ -4,6 +4,8 @@ namespace BabDev\PagerfantaBundle;
 
 use BabDev\PagerfantaBundle\DependencyInjection\BabDevPagerfantaExtension;
 use BabDev\PagerfantaBundle\DependencyInjection\CompilerPass\AddPagerfantasPass;
+use BabDev\PagerfantaBundle\DependencyInjection\CompilerPass\MaybeRemoveTwigViewPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -13,6 +15,7 @@ final class BabDevPagerfantaBundle extends Bundle
     {
         parent::build($container);
 
+        $container->addCompilerPass(new MaybeRemoveTwigViewPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 10);
         $container->addCompilerPass(new AddPagerfantasPass());
     }
 
