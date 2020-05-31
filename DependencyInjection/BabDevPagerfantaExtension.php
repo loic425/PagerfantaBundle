bc89286b4b74cba8f2f032bfbb8580da8f0a3cf8
Michael Babker <michael.babker@gmail.com>
2020-05-31 10:35:04 -0500

    Set deprecated services in the container extension and conditionally use the newer signature

--- DependencyInjection/BabDevPagerfantaExtension.php
+++ DependencyInjection/BabDevPagerfantaExtension.php
@@ -4,12 +4,21 @@ namespace BabDev\PagerfantaBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 final class BabDevPagerfantaExtension extends Extension
 {
+    private const DEPRECATED_VIEW_SERVICES = [
+        'pagerfanta.view.default_translated' => 'default.html.twig',
+        'pagerfanta.view.semantic_ui_translated' => 'semantic_ui.html.twig',
+        'pagerfanta.view.twitter_bootstrap_translated' => 'twitter_bootstrap.html.twig',
+        'pagerfanta.view.twitter_bootstrap3_translated' => 'twitter_bootstrap3.html.twig',
+        'pagerfanta.view.twitter_bootstrap4_translated' => 'twitter_bootstrap.4html.twig',
+    ];
+
     public function getAlias()
     {
         return 'babdev_pagerfanta';
@@ -50,6 +59,25 @@ final class BabDevPagerfantaExtension extends Extension
                 );
         } else {
             $container->removeDefinition('pagerfanta.event_listener.convert_not_valid_current_page_to_not_found');
+        }
+
+        $usesSymfony51Api = method_exists(Definition::class, 'getDeprecation');
+
+        foreach (self::DEPRECATED_VIEW_SERVICES as $serviceId => $replacementTemplate) {
+            $service = $container->getDefinition($serviceId);
+
+            if ($usesSymfony51Api) {
+                $service->setDeprecated(
+                    'babdev/pagerfanta-bundle',
+                    '2.2',
+                    str_replace('%template%', $replacementTemplate, 'The "%service_id%" service is deprecated and will be removed in BabDevPagerfantaBundle 3.0. Use the "pagerfanta.view.twig" service with the "@BabDevPagerfanta/%template%" template instead.')
+                );
+            } else {
+                $service->setDeprecated(
+                    true,
+                    str_replace('%template%', $replacementTemplate, 'The "%service_id%" service is deprecated and will be removed in BabDevPagerfantaBundle 3.0. Use the "pagerfanta.view.twig" service with the "@BabDevPagerfanta/%template%" template instead.')
+                );
+            }
         }
     }
 }
