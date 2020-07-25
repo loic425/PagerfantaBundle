4772f98e4743c2ae63cc77db98a7a3a896f237fc
Michael Babker <michael.babker@gmail.com>
2020-07-25 16:16:47 -0500

    PagerfantaInterface is no longer deprecated as of 2.4

--- Twig/PagerfantaRuntime.php
+++ Twig/PagerfantaRuntime.php
@@ -3,7 +3,6 @@
 namespace BabDev\PagerfantaBundle\Twig;
 
 use Pagerfanta\Exception\OutOfRangeCurrentPageException;
-use Pagerfanta\Pagerfanta;
 use Pagerfanta\PagerfantaInterface;
 use Pagerfanta\RouteGenerator\RouteGeneratorFactoryInterface;
 use Pagerfanta\RouteGenerator\RouteGeneratorInterface;
@@ -53,16 +52,6 @@ final class PagerfantaRuntime implements RuntimeExtensionInterface
             throw new \InvalidArgumentException(sprintf('The $viewName argument of %s() must be an array, a string, or a null value; a %s was given.', __METHOD__, \gettype($viewName)));
         }
 
-        if (!($pagerfanta instanceof Pagerfanta)) {
-            trigger_deprecation(
-                'babdev/pagerfanta-bundle',
-                '2.1',
-                'The "pagerfanta" Twig function will no longer accept "%1$s" implementations that are not a subclass of "%2$s" as of 3.0. Ensure your pager is a subclass of "%2$s".',
-                PagerfantaInterface::class,
-                Pagerfanta::class
-            );
-        }
-
         $viewName = $viewName ?: $this->defaultView;
 
         return $this->viewFactory->get($viewName)->render($pagerfanta, $this->createRouteGenerator($options), $options);
@@ -73,16 +62,6 @@ final class PagerfantaRuntime implements RuntimeExtensionInterface
      */
     public function getPageUrl(PagerfantaInterface $pagerfanta, int $page, array $options = []): string
     {
-        if (!($pagerfanta instanceof Pagerfanta)) {
-            trigger_deprecation(
-                'babdev/pagerfanta-bundle',
-                '2.1',
-                'The "pagerfanta_page_url" Twig function will no longer accept "%1$s" implementations that are not a subclass of "%2$s" as of 3.0. Ensure your pager is a subclass of "%2$s".',
-                PagerfantaInterface::class,
-                Pagerfanta::class
-            );
-        }
-
         if ($page < 0 || $page > $pagerfanta->getNbPages()) {
             throw new OutOfRangeCurrentPageException("Page '{$page}' is out of bounds");
         }
