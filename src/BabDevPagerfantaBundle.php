d5d76b9cdab602f471e55aca8f8e4e585ebca419
Michael Babker <michael.babker@gmail.com>
2021-03-31 17:11:16 -0500

    Drop the remove Twig services pass, we shouldn't need it since these services are only getting loaded when both pagerfanta/twig and TwigBundle are installed

--- src/BabDevPagerfantaBundle.php
+++ src/BabDevPagerfantaBundle.php
@@ -3,9 +3,7 @@
 namespace BabDev\PagerfantaBundle;
 
 use BabDev\PagerfantaBundle\DependencyInjection\BabDevPagerfantaExtension;
-use BabDev\PagerfantaBundle\DependencyInjection\CompilerPass\MaybeRemoveTwigServicesPass;
 use BabDev\PagerfantaBundle\DependencyInjection\CompilerPass\RegisterPagerfantaViewsPass;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -15,8 +13,6 @@ final class BabDevPagerfantaBundle extends Bundle
     {
         parent::build($container);
 
-        // MaybeRemoveTwigServicesPass must be run before the TwigEnvironmentPass from TwigBundle and RegisterPagerfantaViewsPass
-        $container->addCompilerPass(new MaybeRemoveTwigServicesPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1);
         $container->addCompilerPass(new RegisterPagerfantaViewsPass());
     }
 
